208ba5a537a668e4c2dde1068b3c331a105adaa5
John Rogerson <john.rogerson@ge.com>
2015-08-12 12:58:01 -0700

    Updates to test paths

--- karma.conf.js
+++ karma.conf.js
@@ -25,6 +25,8 @@ module.exports = function(config) {
 
       // source files which will be pulled in with html imports
       {pattern: 'bower_components/polymer/*.html', included: false},
+      {pattern: 'bower_components/test/px-timepicker.html', included: false},
+      {pattern: 'bower_components/test/px-datepicker.html', included: false},
       {pattern: 'bower_components/test/px-time-input.html', included: false},
       {pattern: 'bower_components/test/px-range-field.html', included: false},
       {pattern: 'bower_components/moment/moment.js', included: false},
