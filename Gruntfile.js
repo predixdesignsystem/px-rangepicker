2adbd3af0d510e618fb73add3865ebfedb0aa071
Runn Vermel <runn.vermel@ge.com>
2015-09-24 10:18:37 -0700

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,134 @@
+'use strict';
+
+
+module.exports = function(grunt) {
+
+  var importOnce = require('node-sass-import-once');
+  // Project configuration.
+  grunt.initConfig({
+
+    clean: {
+      css: ['css'],
+      bower: ['bower_components'],
+      reports: ['reports']
+    },
+
+    sass: {
+        options: {
+            importer: importOnce,
+            importOnce: {
+              index: true,
+              bower: true
+            }
+        },
+        rangepicker: {
+            files: {
+                'css/noprefix/px-rangepicker-sketch.css': 'sass/px-rangepicker-sketch.scss',
+                'css/noprefix/px-rangepicker.css': 'sass/px-rangepicker-predix.scss'
+            }
+        },
+        timeInput: {
+            files: {
+                'css/noprefix/px-time-input-sketch.css': 'sass/px-time-input-sketch.scss',
+                'css/noprefix/px-time-input.css': 'sass/px-time-input-predix.scss'
+            }
+        },
+        rangeFields: {
+            files: {
+                'css/noprefix/px-range-fields-sketch.css': 'sass/px-range-fields-sketch.scss',
+                'css/noprefix/px-range-fields.css': 'sass/px-range-fields-predix.scss'
+            }
+        },
+        dateRangePicker: {
+            files: {
+                'css/noprefix/px-date-rangepicker-sketch.css': 'sass/px-date-rangepicker-sketch.scss',
+                'css/noprefix/px-date-rangepicker.css': 'sass/px-date-rangepicker-predix.scss'
+            }
+        },
+        timeRangePicker: {
+            files: {
+                'css/noprefix/px-time-rangepicker-sketch.css': 'sass/px-time-rangepicker-sketch.scss',
+                'css/noprefix/px-time-rangepicker.css': 'sass/px-time-rangepicker-predix.scss'
+            }
+        },
+        calendar: {
+            files: {
+                'css/noprefix/px-calendar-sketch.css': 'sass/px-calendar-sketch.scss',
+                'css/noprefix/px-calendar.css': 'sass/px-calendar-predix.scss'
+            }
+        }
+    },
+
+    autoprefixer: {
+      options: {
+        browsers: ['last 3 version']
+      },
+      multiple_files: {
+        expand: true,
+        flatten: true,
+        src: 'css/noprefix/*.css',
+        dest: 'css'
+      }
+    },
+
+    shell: {
+      options: {
+        stdout: true,
+        stderr: true
+      },
+      bower: {
+        command: 'bower install'
+      }
+    },
+
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'js/**/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    watch: {
+      sass: {
+        files: ['sass/**/*.scss'],
+        tasks: ['sass', 'autoprefixer'],
+        options: {
+          interrupt: true
+        }
+      }
+    },
+
+    depserve: {
+      options: {
+        open: '<%= depserveOpenUrl %>'
+      }
+    }
+
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  // Default task.
+  grunt.registerTask('default', 'Basic build', [
+    'sass',
+    'autoprefixer'
+  ]);
+
+  // First run task.
+  grunt.registerTask('firstrun', 'Basic first run', function() {
+    grunt.config.set('depserveOpenUrl', '/index.html');
+    grunt.task.run('default');
+    grunt.task.run('depserve');
+  });
+
+  grunt.registerTask('release', 'Release', [
+    'clean',
+    'shell:bower',
+    'default',
+    'test'
+  ]);
+
+};
---
+++ README.md
@@ -0,0 +1,6 @@
+#### Latest Release
+- not released yet!
+
+#### Active Development (master branch)
+- <a href="http://pxc-demos.grc-apps.svc.ice.ge.com/bower_components/px-rangepicker/demo.html" target="_blank">Demo</a>
+- <a href="http://pxc-demos.grc-apps.svc.ice.ge.com/bower_components/px-rangepicker/index.html" target="_blank">API Docs</a>
