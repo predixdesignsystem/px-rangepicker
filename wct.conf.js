37a464f523e707da693b8b24be42f9580a70ea4c
Benoit Chevalier <benoit.chevalier@ge.com>
2016-04-27 14:24:50 -0700

    timezone for sauce

--- wct.conf.js
+++ wct.conf.js
@@ -9,20 +9,24 @@ module.exports = {
       "browsers": [{
           "browserName": "microsoftedge",
           "platform": "Windows 10",
-          "version": ""
+          "version": "",
+          "timeZone": "Los Angeles"
         }, {
           "browserName": "internet explorer",
           "platform": "Windows 8.1",
-          "version": "11"
+          "version": "11",
+          "timeZone": "Los Angeles"
         },
         {
           "browserName": "safari",
           "platform": "OS X 10.11",
-          "version": "9"
+          "version": "9",
+          "timeZone": "Los Angeles"
         }, {
           "browserName": "safari",
           "platform": "OS X 10.10",
-          "version": "8"
+          "version": "8",
+          "timeZone": "Los Angeles"
         }
       ]
     }
